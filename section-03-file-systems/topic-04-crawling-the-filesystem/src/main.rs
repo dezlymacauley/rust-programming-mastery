74edd3f737c8373d542b6017258bfd80614dfa54
dezlymacauley <dezlymacauley@proton.me>
2026-01-26 09:27:14 +0200

    Started deep dive on fs::read_dir()

--- section-03-file-systems/topic-04-crawling-the-filesystem/src/main.rs
+++ section-03-file-systems/topic-04-crawling-the-filesystem/src/main.rs
@@ -1,5 +1,31 @@
 use std::env;
+use std::fs::read_dir;
 use std::path::Path;
+
+// Accepts a refences to a Path. 
+// Path::new() returns this data type
+fn walk_path(path: &Path) {
+
+    // Assume the path is a directory for this example
+    for entry in read_dir(path).unwrap() {
+
+        let entry = entry.unwrap();
+
+        println!("Entry Path: {}", entry.path().display());
+
+        let path = entry.path();
+
+        if path.is_dir() {
+            // Using recursion to go a level deeper by calling
+            // this function again.
+            walk_path(&path);
+        } else {
+            // If the path is not a directory then print it out.
+            println!("{}", path.display());
+        }
+    }
+
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,5 +40,7 @@ fn main() {
     if !path.exists() {
         println!("Path {} does not exist", path.display());
     }
+
+    walk_path(path);
 
 }
