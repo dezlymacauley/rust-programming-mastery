51c2d98a182387fd88ca4cf0fae58b882bef39a1
dezlymacauley <dezlymacauley@proton.me>
2026-01-27 20:53:13 +0200

    Started with Linked Lists

---
+++ section-07-data-structures/topic-01-singly-linked-list/src/main.rs
@@ -0,0 +1,26 @@
+/*
+    ABOUT: Linked List
+
+    You can't access an element by index.
+
+    Each element is spread in different places in memory.
+
+    A linked list is made up of nodes.
+
+    There is a variable called `Head` that points to the first node.
+
+    There is a variable called Tail that points to the last node.
+
+    Each node contains data, and a pointer to the next node.
+
+    Each node is a heap allocated element.
+
+    The last node contains data, and a nullptr (A pointer to nothing. 
+    which indicates that this is the end of the list.)
+
+*/
+
+use topic_01_singly_linked_list::Node;
+
+fn main() {
+}
