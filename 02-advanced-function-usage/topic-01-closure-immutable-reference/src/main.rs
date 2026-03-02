f2287343d8d091ec0886b600d5ed5b6bec25c62f
dezlymacauley <dezlymacauley@proton.me>
2026-03-02 12:36:22 +0200

    Lesson on closures

---
+++ 02-advanced-function-usage/topic-01-closure-immutable-reference/src/main.rs
@@ -0,0 +1,135 @@
+/*
+    ABOUT: Closure - Immutable reference
+
+    A closure is a function that can automatically use variables from
+    the scope that it was declared in,
+    without having to pass in those variables as parameters.
+
+    Unlike a standard function, a closure is declared inside another function
+    like `fn main()`
+
+    //_________________________________________________________________________
+
+    There are three ways that a closure can automatically use variables
+    from the scope that is was declared in:
+
+    Note: `T` is a placeholder for whatever variable type
+    you need to work with.
+
+    1. By reference (&T) → immutable borrow → Fn
+    2. By mutable reference (&mut T) → mutable borrow → FnMut
+    3. By move (ownership) (T) → takes ownership → FnOnce
+
+    Note: For FnOnce, that closure can only be called once.
+
+    //_________________________________________________________________________
+
+    Closures are essential to understanding intermediate Rust topics,
+    advanced Rust topics like concurrency,
+
+    and working with external packages and frameworks which use closures.
+
+    // The syntax is:
+    // let name_of_closure = | parameters | -> return types {};
+    // How to use a closure
+
+    //_________________________________________________________________________
+*/
+
+fn main() {
+    // These variable will be captured (used) by the closures below.
+    let player_score: i32 = 30;
+    let style_score: i32 = 10;
+
+    let ninja_stars: i32 = 8;
+    let smoke_bombs: i32 = 12;
+
+    //_________________________________________________________________________
+
+    // SECTION: Example 1 (No parameters, no return types)
+
+    let print_player_score = || {
+        println!("player_score: {player_score}");
+    };
+
+    print_player_score();
+    // player_score: 30
+
+    //_________________________________________________________________________
+
+    // SECTION: Example 2 (Single parameter, no return types)
+
+    let print_total_score = |bonus: i32| {
+        let total_score: i32 = player_score + bonus;
+        println!("total_score: {total_score}");
+    };
+
+    println!("player_score: {player_score}");
+    print_total_score(10);
+    // player_score: 30
+    // total_score: 40
+
+    //_________________________________________________________________________
+
+    // SECTION: Example 3 (Multiple parameters, no return types)
+
+    let print_total_weapons = |num_swords: i32, num_guns: i32| {
+        // The variable ninja_stars is captured as an immutable reference
+        let total_weapons: i32 = ninja_stars + num_swords + num_guns;
+        println!("total_weapons: {total_weapons}");
+    };
+
+    println!("ninja_stars: {ninja_stars}");
+    print_total_weapons(2, 4);
+    // ninja_stars: 8
+    // total_weapons: 14
+
+    //_________________________________________________________________________
+
+    // SECTION: Example 4 (Single return type)
+
+    let double_style_score = || -> i32 {
+        // The variable style_score is captured as an immutable reference
+        let doubled_styled_score = style_score * 2;
+        return doubled_styled_score;
+    };
+
+    let variable_a: i32 = double_style_score();
+    println!("variable_a: {variable_a}");
+    // variable_a: 20
+
+    // Since style_score was capturee  as an immutable reference,
+    // the variable  `style_score` was not be modified.
+    println!("style_score: {style_score}");
+    // style_score: 10
+
+    //_________________________________________________________________________
+
+    // SECTION: Example 5 (Multiple return types)
+
+    let double_ninja_weapons = || -> (i32, i32) {
+        // The variables `ninja_stars` and `smoke_bombs`
+        // are captured as an immutable references
+
+        let values: (i32, i32) = (ninja_stars * 2, smoke_bombs * 2);
+
+        return values;
+    };
+
+    let (double_ninja_stars, double_smoke_bombs) = double_ninja_weapons();
+
+    println!("double_ninja_stars: {double_ninja_stars}");
+    println!("double_smoke_bombs: {double_smoke_bombs}");
+    // double_ninja_stars: 16
+    // double_smoke_bombs: 24
+
+    // Since the `ninja_stars` and `smoke_bombs` were captured as immutable
+    // references, the original values were not modified by the closure.
+
+    println!("ninja_stars: {ninja_stars}");
+    println!("smoke_bombs: {smoke_bombs}");
+    // ninja_stars: 8
+    // smoke_bombs: 12
+
+    //_________________________________________________________________________
+}
---
+++ 03-concurrent-programming/theory.md
@@ -0,0 +1,62 @@
+_______________________________________________________________________________
+
+concurrency = managing tasks simulataneously. These task may or may not
+be running at the same time. Concurrency involves tasks switching.
+Making some progress on a task, 
+then pausing and switching another task and the cycle continues until all
+tasks have been completed.
+
+Concurrency is required for creating real world applications, 
+because there are situations where it is simply impractical for the whole
+program to pause and wait for another part of the program to continue.
+
+In programming concurrency (the task switching) happens rapidly so it creates
+the illusion that these tasks are all progressing at the same time.
+
+Concurrency is different but related to concepts like parallelism 
+and asynchronus programming.
+
+_______________________________________________________________________________
+
+parallelism = simulataneous execution of tasks
+
+This when all tasks are being executed at the same time. 
+E.g. multiple workers, dedicated to the completion of each task.
+
+parallelism requires a CPU with multiple cores. Each cores acts as a worker.
+
+So parallelism is about the division of work.
+
+_______________________________________________________________________________
+
+asynchronus = independent task execution
+Some tasks runs separately from the main thread. E.g. API calls.
+
+Mechanisms
+
+threads
+
+messaging channels (communication between threads)
+
+shared state - mutex (mutal exclusion lock) 
+and atomic types (multiple parts of the program reading
+and writing to the same data)
+
+In Rust mutexes are smart. So when a mutex goes out of scope, 
+it automatically releases its lock.
+
+Atomic types = Used when you need to perform simpile operations 
+on shared data without locking.
+_______________________________________________________________________________
+
+concurrent code should be safe and easy to understand
+
+_______________________________________________________________________________
+
+### Issues to watch out for
+
+data races and dead locks
+
+Rust's concurrency model ensures that data is only owned by one thread 
+at a time.
+_______________________________________________________________________________
