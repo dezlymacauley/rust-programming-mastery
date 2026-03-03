8973121dd969da87395d0ba43f045ad7b94b4184
dezlymacauley <dezlymacauley@proton.me>
2026-03-03 12:42:10 +0200

    Improved understanding of recv

---
+++ 03-concurrent-programming/topic-06-shared-state/src/main.rs
@@ -0,0 +1,16 @@
+/*
+    ABOUT: Shared State
+
+    Mutexes
+
+    atomic types (lock-free approach to concurrent programming)
+
+    //_________________________________________________________________________ 
+   
+    Thread safety and data integrity
+
+    //_________________________________________________________________________ 
+
+*/
+
+fn main() {}
